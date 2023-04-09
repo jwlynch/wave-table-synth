05a0af50d4e80531ab4d01342802bfc806395365
Jim <jim@jam.sessionsnet.org>
2023-04-09 08:58:35 -0700

    add code involving pyaudio, but comment it all out

--- wav-info.py
+++ wav-info.py
@@ -14,6 +14,14 @@ wf_name = sys.argv[1]
 
 wf = wave.open(wf_name, 'rb')
 
+#p = pyaudio.PyAudio()
+
+#stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+#                channels=wf.getnchannels(),
+#                rate=wf.getframerate(),
+#                output=True)
+
+#format = p.get_format_from_width(wf.getsampwidth())
 width = wf.getsampwidth()
 channels = wf.getnchannels()
 rate = wf.getframerate()
@@ -21,3 +29,14 @@ rate = wf.getframerate()
 print(f"wave file '{wf_name}', width {width}, channels {channels}, rate {rate}\n")
 
 wf.close()
+
+#data = wf.readframes(CHUNK)
+
+#while data != '':
+#    stream.write(data)
+#    data = wf.readframes(CHUNK)
+
+#stream.stop_stream()
+#stream.close()
+
+#p.terminate()
